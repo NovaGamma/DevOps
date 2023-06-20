63e5bf03cb312178b41c6372080a9789858a0518
NovaGamma <gammanova41@gmail.com>
2023-06-20 11:01:32 +0200

    Fix docker config and remove use of jenkins conf backup

--- VM2/jenkins.sh
+++ VM2/jenkins.sh
@@ -32,8 +32,8 @@ sudo /var/lib/dpkg/info/ca-certificates-java.postinst configure
 /usr/bin/printf '\xfe\xed\xfe\xed\x00\x00\x00\x02\x00\x00\x00\x00\xe2\x68\x6e\x45\xfb\x43\xdf\xa4\xd9\x92\xdd\x41\xce\xb6\xb2\x1c\x63\x30\xd7\x92' > /etc/ssl/certs/java/cacerts
 /var/lib/dpkg/info/ca-certificates-java.postinst configure
 
-echo "Copy jenkins configuration"
-sudo cp -rfT /vagrant/backup/jenkins /var/lib/
+#echo "Copy jenkins configuration"
+#sudo cp -rfT /vagrant/backup/jenkins /var/lib/jenkins
 
 echo "Restart jenkins"
 sudo systemctl restart jenkins
